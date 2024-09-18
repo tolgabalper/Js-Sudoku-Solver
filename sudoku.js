e78afe7b24b9302fd84a6fb5d62ffc8cf618957d
Tolga Boran Alper <60055681+tolgabalper@users.noreply.github.com>
2024-09-18 09:16:41 +0000

    School 1

--- sudoku.js
+++ sudoku.js
@@ -6,6 +6,65 @@ let HorizontalLine = {"Line1":[],"Line2":[],"Line3":[],"Line4":[],"Line5":[],"Li
 let VertialLine = {"Line1":[],"Line2":[],"Line3":[],"Line4":[],"Line5":[],"Line6":[],"Line7":[],"Line8":[],"Line9":[]};
 let ThreeThree = {"First":[],"Second":[],"Third":[],"Fourth":[],"Fifth":[],"Sixth":[],"Seventh":[],"Eighth":[],"Ninth":[]};
 let note = '{"A1":[],"A2":[],"A3":[],"A4":[],"A5":[],"A6":[],"A7":[],"A8":[],"A9":[],"B1":[],"B2":[],"B3":[],"B4":[],"B5":[],"B6":[],"B7":[],"B8":[],"B9":[],"C1":[],"C2":[],"C3":[],"C4":[],"C5":[],"C6":[],"C7":[],"C8":[],"C9":[],"D1":[],"D2":[],"D3":[],"D4":[],"D5":[],"D6":[],"D7":[],"D8":[],"D9":[],"E1":[],"E2":[],"E3":[],"E4":[],"E5":[],"E6":[],"E7":[],"E8":[],"E9":[],"F1":[],"F2":[],"F3":[],"F4":[],"F5":[],"F6":[],"F7":[],"F8":[],"F9":[],"G1":[],"G2":[],"G3":[],"G4":[],"G5":[],"G6":[],"G7":[],"G8":[],"G9":[],"H1":[],"H2":[],"H3":[],"H4":[],"H5":[],"H6":[],"H7":[],"H8":[],"H9":[],"I1":[],"I2":[],"I3":[],"I4":[],"I5":[],"I6":[],"I7":[],"I8":[],"I9":[]}';
+
+function getLocalStorageItem(key) {
+    let itemStr = localStorage.getItem(key);
+
+    // If the item doesn't exist, return null
+    if (!itemStr) {
+        return null;
+    }
+
+    let item = JSON.parse(itemStr);
+    let now = new Date().toLocaleString("en-GB", {timeZone: "Europe/Istanbul"});
+    let itemTime = new Date(item.time).getTime();
+    let nowTime = new Date(now).getTime();
+
+    // If the item is more than a day old, delete it and return null
+    if (nowTime - itemTime > 24 * 60 * 60 * 1000) {
+        localStorage.removeItem(key);
+        return null;
+    }
+
+    // Otherwise, return the item
+    return item.value;
+}
+
+function getData(){
+    let input = JSON.parse(inputVal);
+    for(let i = 0; i < rows.length; i++){
+        for(let j = 0; j < cols.length; j++){
+            let result = getLocalStorageItem(rows[i] + cols[j]);
+            let key = rows[i] + cols[j];
+            input[key] = result;
+        }
+    }
+    inputVal = JSON.stringify(input);
+}
+
+function Select(Line){
+    let ValPerse = JSON.parse(inputVal)
+    let ValLength = Object.keys(ValPerse)
+    if (Line == "Horizontal"){
+        for(let i = 1; i <= ValLength.length; i++) {
+           let key = String(ValLength[i - 1]) 
+            HorizontalLine["Line" + Math.ceil(i / 9)].push(ValPerse[key]);
+        }
+    } else if (Line == "Vertical"){
+        for (let i = 0; i < ValLength.length; i++) {
+            let key = String(ValLength[i -1])
+            VertialLine["Line" + Math.ceil(i/9)].push(ValPerse[key])
+        }
+    }
+}
+
+
+getData()
+
+Select("Horizontal")
+Select("Vertica")
+
+/*
 // Call this function when you want to get a value
 function getLocalStorageItem(key) {
     let itemStr = localStorage.getItem(key);
@@ -102,6 +161,7 @@ function Solver(Row, RS1, RS2, CS1, CS2, SS){
 
 getData();
 Solver(1,0,1,0,9,HorizontalLine["Line1"])
+*/
 
 /*
 Selector(0,1,0,1,ThreeThree["First"]);
