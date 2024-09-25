428ed1c6b0ea2638dae080d545f6e719054938fd
Tolga Boran Alper <60055681+tolgabalper@users.noreply.github.com>
2024-09-25 09:16:24 +0000

    School 2 // Vertical select fixed - ThreeThree for first 3 added

--- sudoku.js
+++ sudoku.js
@@ -4,7 +4,7 @@ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 let inputVal = '{"A1":"","A2":"","A3":"","A4":"","A5":"","A6":"","A7":"","A8":"","A9":"","B1":"","B2":"","B3":"","B4":"","B5":"","B6":"","B7":"","B8":"","B9":"","C1":"","C2":"","C3":"","C4":"","C5":"","C6":"","C7":"","C8":"","C9":"","D1":"","D2":"","D3":"","D4":"","D5":"","D6":"","D7":"","D8":"","D9":"","E1":"","E2":"","E3":"","E4":"","E5":"","E6":"","E7":"","E8":"","E9":"","F1":"","F2":"","F3":"","F4":"","F5":"","F6":"","F7":"","F8":"","F9":"","G1":"","G2":"","G3":"","G4":"","G5":"","G6":"","G7":"","G8":"","G9":"","H1":"","H2":"","H3":"","H4":"","H5":"","H6":"","H7":"","H8":"","H9":"","I1":"","I2":"","I3":"","I4":"","I5":"","I6":"","I7":"","I8":"","I9":""}';
 let HorizontalLine = {"Line1":[],"Line2":[],"Line3":[],"Line4":[],"Line5":[],"Line6":[],"Line7":[],"Line8":[],"Line9":[]};
 let VertialLine = {"Line1":[],"Line2":[],"Line3":[],"Line4":[],"Line5":[],"Line6":[],"Line7":[],"Line8":[],"Line9":[]};
-let ThreeThree = {"First":[],"Second":[],"Third":[],"Fourth":[],"Fifth":[],"Sixth":[],"Seventh":[],"Eighth":[],"Ninth":[]};
+let ThreeThree = {"1":[],"2":[],"3":[],"4":[],"5":[],"6":[],"7":[],"8":[],"9":[]};
 let note = '{"A1":[],"A2":[],"A3":[],"A4":[],"A5":[],"A6":[],"A7":[],"A8":[],"A9":[],"B1":[],"B2":[],"B3":[],"B4":[],"B5":[],"B6":[],"B7":[],"B8":[],"B9":[],"C1":[],"C2":[],"C3":[],"C4":[],"C5":[],"C6":[],"C7":[],"C8":[],"C9":[],"D1":[],"D2":[],"D3":[],"D4":[],"D5":[],"D6":[],"D7":[],"D8":[],"D9":[],"E1":[],"E2":[],"E3":[],"E4":[],"E5":[],"E6":[],"E7":[],"E8":[],"E9":[],"F1":[],"F2":[],"F3":[],"F4":[],"F5":[],"F6":[],"F7":[],"F8":[],"F9":[],"G1":[],"G2":[],"G3":[],"G4":[],"G5":[],"G6":[],"G7":[],"G8":[],"G9":[],"H1":[],"H2":[],"H3":[],"H4":[],"H5":[],"H6":[],"H7":[],"H8":[],"H9":[],"I1":[],"I2":[],"I3":[],"I4":[],"I5":[],"I6":[],"I7":[],"I8":[],"I9":[]}';
 
 function getLocalStorageItem(key) {
@@ -42,6 +42,8 @@ function getData(){
     inputVal = JSON.stringify(input);
 }
 
+
+
 function Select(Line){
     let ValPerse = JSON.parse(inputVal)
     let ValLength = Object.keys(ValPerse)
@@ -51,18 +53,54 @@ function Select(Line){
             HorizontalLine["Line" + Math.ceil(i / 9)].push(ValPerse[key]);
         }
     } else if (Line == "Vertical"){
-        for (let i = 0; i < ValLength.length; i++) {
-            let key = String(ValLength[i -1])
-            VertialLine["Line" + Math.ceil(i/9)].push(ValPerse[key])
+        let key
+        for(let j = 1; j <= 9;  j++){
+                // Filtering keys
+                let filteredKeys = ValLength.filter(key => key.includes(String(j)));
+                for(let k = 1; k <= filteredKeys.length; k++){
+                    key = String(filteredKeys[k -1 ])
+                    VertialLine["Line" + j].push(ValPerse[key])
+                }
         }
+    } else if (Line == "ThreeThree"){
+        let three = ThreeThree
+        let horizontal = HorizontalLine
+        let counter = 0
+        let key
+
+        while (true) {
+            for(counter; counter <= 8; counter++){
+                key = (counter*3) + 2
+                switch (counter) {
+                    case 0:
+                    case 1:
+                    case 2:
+                        for(let i = 1; i <= 3; i++){
+                            for(let k = counter*3; k <= key; k++){
+                                three[String(counter + 1)].push(horizontal["Line" + String(i)][k])
+                            }
+                        }
+                        break;
+                
+                    default:
+                        break;
+                }
+            }
+            break
+        }
+
+
     }
+
+
 }
 
 
 getData()
 
 Select("Horizontal")
-Select("Vertica")
+Select("Vertical")
+Select("ThreeThree")
 
 /*
 // Call this function when you want to get a value
