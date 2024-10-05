bbee6dc7636d7288db951d6996c781068f00652e
Tolga Boran Alper <60055681+tolgabalper@users.noreply.github.com>
2024-10-05 15:37:45 +0000

    School 3

--- sudoku.js
+++ sudoku.js
@@ -6,6 +6,7 @@ let HorizontalLine = {"Line1":[],"Line2":[],"Line3":[],"Line4":[],"Line5":[],"Li
 let VertialLine = {"Line1":[],"Line2":[],"Line3":[],"Line4":[],"Line5":[],"Line6":[],"Line7":[],"Line8":[],"Line9":[]};
 let ThreeThree = {"1":[],"2":[],"3":[],"4":[],"5":[],"6":[],"7":[],"8":[],"9":[]};
 let note = '{"A1":[],"A2":[],"A3":[],"A4":[],"A5":[],"A6":[],"A7":[],"A8":[],"A9":[],"B1":[],"B2":[],"B3":[],"B4":[],"B5":[],"B6":[],"B7":[],"B8":[],"B9":[],"C1":[],"C2":[],"C3":[],"C4":[],"C5":[],"C6":[],"C7":[],"C8":[],"C9":[],"D1":[],"D2":[],"D3":[],"D4":[],"D5":[],"D6":[],"D7":[],"D8":[],"D9":[],"E1":[],"E2":[],"E3":[],"E4":[],"E5":[],"E6":[],"E7":[],"E8":[],"E9":[],"F1":[],"F2":[],"F3":[],"F4":[],"F5":[],"F6":[],"F7":[],"F8":[],"F9":[],"G1":[],"G2":[],"G3":[],"G4":[],"G5":[],"G6":[],"G7":[],"G8":[],"G9":[],"H1":[],"H2":[],"H3":[],"H4":[],"H5":[],"H6":[],"H7":[],"H8":[],"H9":[],"I1":[],"I2":[],"I3":[],"I4":[],"I5":[],"I6":[],"I7":[],"I8":[],"I9":[]}';
+let LineKey = [2, 5, 8]
 
 function getLocalStorageItem(key) {
     let itemStr = localStorage.getItem(key);
@@ -44,7 +45,7 @@ function getData(){
 
 
 
-function Select(Line){
+function LineSelect(Line){
     let ValPerse = JSON.parse(inputVal)
     let ValLength = Object.keys(ValPerse)
     if (Line == "Horizontal"){
@@ -66,24 +67,50 @@ function Select(Line){
         let three = ThreeThree
         let horizontal = HorizontalLine
         let counter = 0
-        let key
+        let key = 0
 
-        while (true) {
+        while (key <= 3) {
             for(counter; counter <= 8; counter++){
-                key = (counter*3) + 2
                 switch (counter) {
                     case 0:
                     case 1:
                     case 2:
                         for(let i = 1; i <= 3; i++){
-                            for(let k = counter*3; k <= key; k++){
+                            for(let k = counter*3; k <= LineKey[key]; k++){
                                 three[String(counter + 1)].push(horizontal["Line" + String(i)][k])
                             }
                         }
+
+                        key++
+                        if (key == 3) {
+                            key = 0
+                        }
                         break;
-                
-                    default:
+                    case 3:
+                    case 4:
+                    case 5:
+                        for(let i = 4; i <= 6; i++){
+                            for(let k = (counter - 3)*3; k <= LineKey[key]; k++){
+                                three[String(counter + 1)].push(horizontal["Line" + String(i)][k])
+                            }
+                        }
+                        key++
+                        if (key == 3) {
+                            key = 0
+                        }
                         break;
+                    case 6:
+                    case 7:
+                    case 8:
+                        for(let i = 7; i <= 9;i++){
+                            for(let k = (counter - 6)*3; k <= LineKey[key]; k++){
+                                three[String(counter + 1)]. push(horizontal["Line" + String(i)][k])
+                            }
+                        }
+                        key++
+                        if(key == 2 & counter == 8){
+                            key = 4
+                        }
                 }
             }
             break
@@ -95,12 +122,26 @@ function Select(Line){
 
 }
 
+function NoteTaking(Area) {
+    if(Area == "Horizontal"){
+        let horizontal = HorizontalLine
+        for(let i = 1; i <= 9; i++){
+            for(let k = 0; k <= 8; k++){
+                let selected = horizontal["Line" + i][k]
+                if(selected == "0"){
+                    parseInt(horizontal["Line" + i])
+                    
+                }
+            }
+        }
+    }
+}
 
 getData()
 
-Select("Horizontal")
-Select("Vertical")
-Select("ThreeThree")
+LineSelect("Horizontal")
+LineSelect("Vertical")
+LineSelect("ThreeThree")
 
 /*
 // Call this function when you want to get a value
